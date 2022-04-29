10372063441b4a78feedc339052ead06b4fe09dd
daedae2001 <daedae2001@gmail.com>
2022-04-29 15:50:09 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask,  render_template, request, session #, redirect, url_for,jsonify # pip install Flask
+#from flask_mysqldb import MySQL,MySQLdb # pip install Flask-MySQLdb
+from os import path 
+#from notifypy import Notify #pip install notify-py
+
+app = Flask(__name__)
+"""app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'dae'
+app.config['MYSQL_PASSWORD'] = 'Asd13579+'
+app.config['MYSQL_DB'] = 'iptv'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+"""
+@app.route('/')
+def home():
+    return render_template("contenido.html")    
+
+@app.route('/layout', methods = ["GET", "POST"])
+def layout():
+    session.clear()
+    return render_template("contenido.html")
+
+@app.route('/actualiza/')
+def actualiza():
+    import descarga
+    import crea_guia_html
+    descarga.ejecuta_descarga()
+    crea_guia_html.crea_grilla()
+    return render_template('premium/home.html')
+@app.route('/tv', methods = ["GET", "POST"])
+def tv():
+    act = request.args.get('act', default = 0, type = int)
+    if act == 0:
+        return render_template('tv.html')
+    else:
+        import descarga
+        import crea_guia_html
+        descarga.ejecuta_descarga()
+        crea_guia_html.crea_grilla()
+        return render_template('tv.html')
+   
+if __name__ == '__main__':
+    app.secret_key = "PFEh8!YR7C&xL4"
+    app.run(debug=True)
