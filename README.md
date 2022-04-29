cd0d7c268a4dbb934b6de5f2ed1be1ccdf6fe8ff
daedae2001 <daedae2001@gmail.com>
2022-04-29 15:57:26 +0200

    a

---
+++ README.md
@@ -0,0 +1,21 @@
+Hola...
+
+
+En este repositorio encontraras una aplicación web con temática de citas en linea, se hace uso del Framework Flask para la creación de un Login y SignUp, con conexión a una base de datos, siendo total mente funcional.
+
+Espero que te sea de gran ayuda y logres aprender.
+
+Librerias a descargar
++ Framework Flask --- pip install Flask
++ MySql           --- pip install Flask-MySQLdb
++ Notificaciones  --- pip install notify-py
+
+
+El archivo app_citas.sql corresponde a la base de datos propuesta para la aplicación, necesaria para su funcionalidad.
+
+
+Video en YouTube: https://youtu.be/2_6Pp9vy864
+
+![image](https://user-images.githubusercontent.com/61909378/149815685-f175cae5-5e84-409b-a846-f9b8dcecc25a.png)
+![image](https://user-images.githubusercontent.com/61909378/149815893-03ce8788-f908-40e7-826d-ca2d8f589be7.png)
+![image](https://user-images.githubusercontent.com/61909378/149815926-b0f2ce62-aafe-4c79-bfde-1cd41cf13ef5.png)
---
+++ descarga.py
@@ -0,0 +1,62 @@
+import requests
+from os import remove
+mylist = [["es.xml", 'https://i.mjh.nz/PlutoTV/es.xml', 'es'],
+          ["mx.xml", 'https://i.mjh.nz/PlutoTV/mx.xml', 'mx'],
+          ["mv.xml", 'https://raw.githubusercontent.com/davidmuma/EPG_dobleM/master/guia.xml', '']]
+mylist_locales = [["aizzi.xml", 'aizzi.xml', '']]
+
+i = 1
+primera = ""
+otras = ""
+
+def insertar_texto(cadena, texto):
+    posicion = cadena.find('<programme channel="')
+    if posicion <= len(cadena):
+        izquierda = cadena[:posicion]
+        derecha = cadena[posicion:]
+
+        return '{} {} {}'.format(izquierda, texto, derecha)
+    else:
+        raise ValueError(
+            'La posición donde se quiere insertar el texto no existe.')
+
+def ejecuta_descarga():
+    i = 1
+
+    for x in mylist:
+        url = x[1]
+        myfile = requests.get(url)
+        open(x[0], 'wb').write(myfile.content)
+        f = open(x[0], 'r', encoding="utf8")
+
+        if i == 1:
+            primera = f.read()
+            f.close()
+            primera = primera.replace('<channel id="', '<channel id="' +
+                                    x[2]).replace('<programme channel="', '<programme channel="'+x[2]).replace("</display-name>", "_"+x[2]+"</display-name>")
+        else:
+            otras = f.read()
+            f.close()
+            otras = otras[otras.find('<channel id="'):]
+            otras = otras.replace('<channel id="', '<channel id="' +
+                                x[2]).replace('<programme channel="', '<programme channel="'+x[2]).replace("</display-name>", "_"+x[2]+"</display-name>")
+            otras = otras.replace("</tv>", "")
+            primera = insertar_texto(primera, otras)
+        
+        i = i+1
+    primera = insertar_texto(primera, otras)
+
+    f = open('todo.xml', 'w', encoding="utf8")
+    f.write(primera.replace('></desc>', '>nn</desc>'))
+    f.close()
+
+
+    try:
+        f = open('todo.xml', "rb")
+    except IOError as e:
+        print(e.errno, e.message)
+    else:
+        data = f.read()
+        f.close()
+ejecuta_descarga()
+
